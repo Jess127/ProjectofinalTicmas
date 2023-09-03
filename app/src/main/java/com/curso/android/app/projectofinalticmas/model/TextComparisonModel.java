86a5ff1b0fad46cf112beab21b89956d69c8611f
Jess127 <126628293+Jess127@users.noreply.github.com>
2023-09-03 19:58:29 -0300

    Delete app directory

--- app/src/main/java/com/curso/android/app/projectofinalticmas/model/TextComparisonModel.java
+++
@@ -1,8 +0,0 @@
-package com.curso.android.app.projectofinalticmas.model;
-
-public class TextComparisonModel {
-    public boolean areTextsEqual(String text1, String text2) {
-        return text1.equalsIgnoreCase(text2);
-    }
-
-}
